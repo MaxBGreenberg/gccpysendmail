01d72c09542764f12d1ac9e0082d5e3d786b5d5e
MaxBGreenberg <maxbgreenberg@gmail.com>
2025-11-26 10:39:33 -0500

    add comments

--- sendmail.py
+++ sendmail.py
@@ -1,10 +1,12 @@
 #!/bin/env python3
 # Script for automating sending of appointment confirmation emails
 # Licensed under GPLv2
+# Import modules
 from datetime import datetime, timedelta
 import smtplib
 from email.mime.text import MIMEText
 import json
+# Prompt for user input
 print("Enter appointment date:")
 apptdate=input()
 print("Enter appointment time (HHMM 24hr format):")
@@ -13,25 +15,33 @@ print("Enter the number of patients:")
 patnum=input()
 print("Enter the email address of the recipient:")
 recipient=input()
+# Adjust grammar of email depending on number of patients specified
 if patnum in ("","0","1"):
 	appt="son's circumcision"
 else:
 	appt="sons' circumcisions"
+# Convert input date and time to date time objects
+# Get arrival time by doing arithmetic on apppointment time
+# Convert appointment time from input fomrat to display format
 appttimeobj=datetime.strptime(appttime, "%H%M")
 arrtimeobj=appttimeobj-timedelta(minutes=15)
 arrtime=arrtimeobj.strftime("%I:%M%p").lstrip("0")
 appttimedisp=appttimeobj.strftime("%I:%M%p").lstrip("0")
+# HTML for body of the email
 body=f"Hello,<br><br>Thank you for booking your {appt} with the <a href='https://drgreenberg.ca'>Greenberg Circumcision Centre</a>.<br>Your appointment is booked for {apptdate} at {appttimedisp}. Please arrive no later than {arrtime}.<br>Please remember to pay by the end of the day today either via the <a href='https://drgreenberg.ca/product/circumcision-services/'>secture web protal</a> or by Interac e-Transfer to <a href='mailto:mark@drgreenberg.ca'>mark@drgreenberg.ca</a>.<br>A map to our location can be found <a href='https://drgreenberg.ca/contact/'>here</a>.<br>Everything else you need to know is on <a href='https://drgreenberg.ca'>our website</a>.<br><br>Thanks,<br>Max"
+# Load email credentials from JSON file
 with open("secrets.json","r") as f:
 	secrets=json.load(f)
 username=secrets["username"]
 password=secrets["password"]
 server_url=secrets["server_url"]
 port=secrets["port"]
+# Define email structure
 msg=MIMEText(body,'html')
 msg['Subject']='Appointment Notification'
 msg['From']=username
 msg['To']=recipient
+# Send email
 with smtplib.SMTP_SSL(server_url,port) as server:
 	server.login(username,password)
 	server.send_message(msg)
